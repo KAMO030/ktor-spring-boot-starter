845a40e3b9dcf34561ecea1f4c2d440f60c8d870
kamosama <837080904@qq.com>
2024-01-05 14:07:18 +0800

    feat: 注册 extensionAdapter 分别适配Module和Router的扩展方法

--- src/main/kotlin/KtorExtensionBeanPostProcessor.kt
+++ src/main/kotlin/KtorExtensionBeanPostProcessor.kt
@@ -19,17 +19,29 @@ import kotlin.reflect.full.extensionReceiverParameter
 @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
 class KtorExtensionBeanPostProcessor : BeanPostProcessor, BeanDefinitionRegistryPostProcessor {
     companion object {
-        private const val BEAN_NAME: String = "extensionBeanPostProcessorModule"
+        private const val MODULE_ADAPTER_BEAN_NAME: String = "extensionBeanPostProcessorModuleAdapter"
+        private const val ROUTER_ADAPTER_BEAN_NAME: String = "extensionBeanPostProcessorRouterAdapter"
         private const val APPLICATION_TYPE_NAME = "io.ktor.server.application.Application"
         private const val ROUTING_TYPE_NAME = "io.ktor.server.routing.Routing"
     }
 
     private val moduleFunctions = mutableListOf<Pair<Any, KFunction<*>>>()
-    private val routingFunctions = mutableListOf<Pair<Any, KFunction<*>>>()
+    private val routerFunctions = mutableListOf<Pair<Any, KFunction<*>>>()
 
+    private val extensionAdapter = object : KtorModule, KtorRouter {
+        override fun Application.install() {
+            moduleFunctions.forEach { it.second.call(it.first, this) }
+        }
+
+        override fun Routing.register() {
+            routerFunctions.forEach {
+                it.second.call(it.first, this)
+            }
+        }
+    }
 
     override fun postProcessBeforeInitialization(bean: Any, beanName: String): Any {
-        if (beanName == BEAN_NAME) {
+        if (beanName == MODULE_ADAPTER_BEAN_NAME || beanName == ROUTER_ADAPTER_BEAN_NAME) {
             return bean
         }
         val extFunctions = runCatching {
@@ -39,28 +51,25 @@ class KtorExtensionBeanPostProcessor : BeanPostProcessor, BeanDefinitionRegistry
         extFunctions.forEach {
             if (it.extensionReceiverParameter?.type?.toString() == APPLICATION_TYPE_NAME && !bean.instanceOf(KtorModule::class)) {
                 moduleFunctions.add(bean to it)
-            }else if (it.extensionReceiverParameter?.type?.toString() == ROUTING_TYPE_NAME && !bean.instanceOf(KtorRouter::class)) {
-                routingFunctions.add(bean to it)
+            } else if (it.extensionReceiverParameter?.type?.toString() == ROUTING_TYPE_NAME && !bean.instanceOf(
+                    KtorRouter::class
+                )
+            ) {
+                routerFunctions.add(bean to it)
             }
         }
         return bean
     }
 
     override fun postProcessBeanDefinitionRegistry(registry: BeanDefinitionRegistry) {
-        val beanDefinition = BeanDefinitionBuilder.rootBeanDefinition(KtorModule::class.java) {
-            object : KtorModule {
-                override fun Application.install() {
-                    moduleFunctions.forEach { it.second.call(it.first, this) }
-                    routing {
-                        routingFunctions.forEach {
-                            it.second.call(it.first, this)
-                        }
-                    }
-                }
-            }
-        }.beanDefinition
-        registry.registerBeanDefinition(BEAN_NAME, beanDefinition)
+        val extensionModuleAdapterBD = BeanDefinitionBuilder
+            .rootBeanDefinition(KtorModule::class.java) { extensionAdapter }.beanDefinition
+        val extensionRouterAdapterBD = BeanDefinitionBuilder
+            .rootBeanDefinition(KtorRouter::class.java) { extensionAdapter }.beanDefinition
+        registry.registerBeanDefinition(MODULE_ADAPTER_BEAN_NAME, extensionModuleAdapterBD)
+        registry.registerBeanDefinition(ROUTER_ADAPTER_BEAN_NAME, extensionRouterAdapterBD)
     }
 
     override fun postProcessBeanFactory(beanFactory: ConfigurableListableBeanFactory) = Unit
+
 }
